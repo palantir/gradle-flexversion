f73f6f2e6fb47999d45ee8168d9ebe2a1aa77955
Konstantinos Niktas <kniktas@palantir.com>
2015-04-21 14:55:43 -0700

    Remove tabs and make em spaces

--- src/main/groovy/com/palantir/gradle/versions/domainversioning/DomainVersionConvention.groovy
+++ src/main/groovy/com/palantir/gradle/versions/domainversioning/DomainVersionConvention.groovy
@@ -4,17 +4,17 @@ import org.gradle.api.Project;
 
 public class DomainVersionConvention {
 
-	private Project project;
-	
-	public DomainVersionConvention(Project project) {
-		this.project = project;
-	}
-	
-	public String domainVersion() {
-		return DomainVersionPlugin.buildDomainVersion(project, null);
-	}
-	
-	public String domainVersion(String userDomain) {
-		return DomainVersionPlugin.buildDomainVersion(project, userDomain);
-	}
+    private Project project;
+
+    public DomainVersionConvention(Project project) {
+        this.project = project;
+    }
+
+    public String domainVersion() {
+        return DomainVersionPlugin.buildDomainVersion(project, null);
+    }
+
+    public String domainVersion(String userDomain) {
+        return DomainVersionPlugin.buildDomainVersion(project, userDomain);
+    }
 }
