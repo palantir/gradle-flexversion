10046c71923a73660c01d962bc04831dbefbc215
Konstantinos Niktas <kniktas@palantir.com>
2015-06-10 13:08:51 -0700

    Tests for custom vars and version objects. Added domain pattern exception
    -Renamed the constants as well

--- src/test/groovy/com/palantir/gradle/versions/flexversioning/FlexVersionTests.groovy
+++ src/test/groovy/com/palantir/gradle/versions/flexversioning/FlexVersionTests.groovy
@@ -15,10 +15,13 @@ import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import com.palantir.gradle.versions.flexversioning.FlexVersionPlugin.FlexVersionPatternException;
+import com.sun.org.apache.xalan.internal.xsltc.cmdline.getopt.IllegalArgumentException;
+
 class FlexVersionTests {
 
-    private static final String projectName = "flex-testing";
-    private static final String tag = "1.5.2";
+    private static final String PROJECT_NAME = "flex-testing";
+    private static final String HEAD_TAG_NAME = "1.5.2";
 
     static File repoFolder;
     static Git git;
@@ -30,7 +33,7 @@ class FlexVersionTests {
 
     @BeforeClass
     public static void setUpRepo() {
-        repoFolder = new File("${System.properties['user.dir']}/${projectName}");
+        repoFolder = new File("${System.properties['user.dir']}/${PROJECT_NAME}");
         repoFolder.deleteDir();
         repoFolder.mkdirs();
         
@@ -56,12 +59,12 @@ class FlexVersionTests {
         RevCommit head = new RevWalk(repo).parseCommit(repo.resolve(Constants.HEAD));
         headShaShort = head.name().substring(0,12);
         
-        git.tag().setAnnotated(true).setName(tag).setMessage("This is the ${tag} release...bugfix!").setObjectId(head).call();
+        git.tag().setAnnotated(true).setName(HEAD_TAG_NAME).setMessage("This is the ${HEAD_TAG_NAME} release...bugfix!").setObjectId(head).call();
     }
     
     @Before
     public void setUpProject() {
-        project = ProjectBuilder.builder().withProjectDir(repoFolder).withName(projectName).build();
+        project = ProjectBuilder.builder().withProjectDir(repoFolder).withName(PROJECT_NAME).build();
         new FlexVersionPlugin().apply(project);
     }
     
@@ -85,13 +88,61 @@ class FlexVersionTests {
     @Test
     public void testTag() {
         project.flexversion.useTags = true
-        assert tag == project.flexVersion().toString()
-        assert tag == project.flexVersion("myownchoice").toString()
+        assert HEAD_TAG_NAME == project.flexVersion().toString()
+        assert HEAD_TAG_NAME == project.flexVersion("myownchoice").toString()
     }
     
     @Test
     public void testEnvironmentBasic() {
         project.flexversion.envvarSources << "buildRef"
-        assert "${System.env['buildRef']}-${commits}-g${headShaShort}" == project.flexVersion().toString()
+        assert "${System.env['buildRef'].replace('/','-')}-${commits}-g${headShaShort}" == project.flexVersion().toString()
+    }
+
+    @Test
+    public void testEnvironmentOrder() {
+        project.flexversion.envvarSources << "GIT_BRANCH_FOR_TESTING_CLEAN" << "buildRef"
+        assert "${System.env['GIT_BRANCH_FOR_TESTING_CLEAN'].replace('/','-')}-${commits}-g${headShaShort}" == project.flexVersion().toString()
+    }
+
+    @Test
+    public void testEnvironmentStandardStrip() {
+        project.flexversion.envvarSources << "GIT_BRANCH_FOR_TESTING"
+        assert "${System.env['GIT_BRANCH_FOR_TESTING_CLEAN'].replace('/','-')}-${commits}-g${headShaShort}" == project.flexVersion().toString()
+    }
+
+    @Test
+    public void testEnvironmentCustomStrip() {
+        project.flexversion {
+            envvarSources << "git.branch"
+            stripRefs << "gerrit/"
+        }
+        assert "${System.env['git.branch_CLEAN'].replace('/','-')}-${commits}-g${headShaShort}" == project.flexVersion().toString()
+    }
+
+    @Test(expected=FlexVersionPatternException.class)
+    public void testDomainPattern() {
+        project.flexversion.domainPattern = ~/\d+\.\d+\.\d+/
+        project.flexVersion()
+    }
+
+    @Test
+    public void testVersionObject() {
+        FlexVersion version = project.flexVersion("objecttest")
+        assert version.domain == "objecttest"
+        assert version.commitCount == commits
+        assert version.gitHash == headShaShort
+        assert version.tag == false
+        assert version.toString() == "objecttest-${commits}-g${headShaShort}"
+    }
+
+    @Test
+    public void testVersionObjectWithTag() {
+        project.flexversion.useTags = true
+        FlexVersion version = project.flexVersion("objecttest")
+        assert version.domain == HEAD_TAG_NAME
+        assert version.commitCount == commits
+        assert version.gitHash == headShaShort
+        assert version.tag == true
+        assert version.toString() == HEAD_TAG_NAME
     }
 }
