aa49338ffb2a1af313fd3c5e6db0985add3fb240
Konstantinos Niktas <kniktas@palantir.com>
2015-06-09 18:05:06 -0700

    Clean up the set up for tests. Make values accessible for testing

--- src/test/groovy/com/palantir/gradle/versions/flexversioning/FlexVersionTests.groovy
+++ src/test/groovy/com/palantir/gradle/versions/flexversioning/FlexVersionTests.groovy
@@ -16,37 +16,50 @@ import org.junit.Test;
 
 class FlexVersionTests {
 
+    private static final String projectName = "flex-testing";
+    private static final String tag = "1.5.2";
 
     static File repoFolder;
     static Git git;
     static Repository repo;
-    
+    static String headSha;
+    static int commits = 0;
+
     Project project;
 
     @BeforeClass
     public static void setUpRepo() {
-        repoFolder = new File("${System.properties['user.dir']}/flex-testing");
+        repoFolder = new File("${System.properties['user.dir']}/${projectName}");
         repoFolder.deleteDir();
         repoFolder.mkdirs();
         
         git = Git.init().setDirectory(repoFolder).call();
         repo = new FileRepositoryBuilder().findGitDir(repoFolder).build();
         
-        new File("${repoFolder}/testing").append("Some content");
-        git.add().addFilepattern("testing").call();
-        git.commit().setMessage("WOW").call();
+        def commitMessages = [
+                "Initial commit",
+                "We can keep adding commits",
+                "Add some more information",
+                "This is a refactor",
+                "Final commit"
+            ]
+        
+        for (message in commitMessages) {
+            new File("${repoFolder}/myfile.txt").append("${System.currentTimeMillis()} - ${message}\n");
+            git.add().addFilepattern("myfile.txt").call();
+            git.commit().setMessage(message).call();
+            commits++;
+        }
         
         RevCommit head = new RevWalk(repo).parseCommit(repo.resolve(Constants.HEAD));
-        println "HEAD " + head.name();
+        headSha = head.name();
         
-        git.tag().setAnnotated(true).setName("0.642.0").setMessage("This is 0.642.0 wow!").setObjectId(null).call();
-        
-        
+        git.tag().setAnnotated(true).setName(tag).setMessage("This is the ${tag} release...bugfix!").setObjectId(head).call();
     }
     
     @Before
     public void setUpProject() {
-        project = ProjectBuilder.builder().withProjectDir(repoFolder).withName("flex-test").build();
+        project = ProjectBuilder.builder().withProjectDir(repoFolder).withName(projectName).build();
         new FlexVersionPlugin().apply(project);
     }
     
